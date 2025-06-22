c73e446a053385dacaf7a6f5b9bfeda35f88e30f
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-06-22 20:02:19 +0330

    create show more component

---
+++ lib/components/ShowMore/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ArrowDown } from "lucide-react";
+import { ReactNode, useState } from "react";
+
+// utils
+import { cn } from "@app/utils";
+
+// locals
+import { IconButton } from "..";
+
+interface ShowMoreProps {
+  children: ReactNode;
+}
+export const ShowMore = ({ children }: ShowMoreProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="relative flex flex-col items-center">
+      <div
+        className={cn(
+          "overflow-y-hidden transition-[max-height] duration-300 ease-in-out",
+          isOpen ? "max-h-[5000px]" : "max-h-64",
+        )}
+      >
+        {children}
+      </div>
+
+      {!isOpen && (
+        <div className="from-background pointer-events-none absolute right-0 bottom-0 left-0 h-42 bg-gradient-to-t to-transparent" />
+      )}
+      {!isOpen && (
+        <IconButton className="bg-background absolute bottom-0 animate-bounce cursor-pointer rounded-full">
+          <ArrowDown onClick={() => setIsOpen(true)} />
+        </IconButton>
+      )}
+    </div>
+  );
+};
--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,4 +1,7 @@
 export * from "./MdxViewer";
 export * from "./PostCard";
 export * from "./ProjectCard";
+export * from "./Timeline";
+export * from "./ShowMore";
+export * from "./IconButton";
 export * from "./UI";
