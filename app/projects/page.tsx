a184ba2624f145e87b3135ba44247be87aca7e5a
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-06-22 14:32:17 +0330

    update cards list in projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,12 +14,11 @@ const ProjectsPage = () => {
     <div className="animate-fade-in flex flex-col gap-2 md:py-4">
       <h2 className="mx-4 text-xl font-bold">Projects</h2>
       <div className="flex flex-col gap-4 md:gap-1">
-        {projectService.getAll().map((project, index) => {
+        {projectService.getAll().map((project) => {
           return (
             <Link
-              key={index}
+              key={project.url}
               href={routes.SINGLE_PROJECT_ROUTE(project.slug)}
-              target="_blank"
             >
               <ProjectCard project={project} />
             </Link>
