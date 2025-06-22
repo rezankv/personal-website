f1b97835ec6659ff8bab9e5ac002089b2819267d
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-06-22 20:01:58 +0330

    create icon button

---
+++ lib/components/IconButton/index.tsx
@@ -0,0 +1,24 @@
+import { HTMLAttributes } from "react";
+
+// utils
+import { cn } from "@app/utils";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface IconButtonProps extends HTMLAttributes<HTMLButtonElement> {}
+export const IconButton = ({
+  children,
+  className,
+  ...props
+}: IconButtonProps) => {
+  return (
+    <button
+      className={cn(
+        "ring-offset-background focus-visible:ring-ring border-input hover:bg-accent text-muted-foreground hover:text-foreground inline-flex size-10 items-center justify-center rounded-md border bg-transparent whitespace-nowrap transition-colors duration-200 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
