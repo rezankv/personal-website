43804e2a5a705594e9ecdd9a0b1357e2392ddfb2
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-06-23 11:22:53 +0330

    cleanup index.css

--- lib/components/IconButton/index.tsx
+++ lib/components/IconButton/index.tsx
@@ -13,7 +13,7 @@ export const IconButton = ({
   return (
     <span
       className={cn(
-        "ring-offset-background focus-visible:ring-ring border-input hover:bg-accent text-muted-foreground hover:text-foreground inline-flex size-10 items-center justify-center rounded-md border bg-transparent whitespace-nowrap transition-colors duration-200 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
+        "ring-offset-background  border-border hover:bg-muted text-muted-foreground hover:text-foreground inline-flex size-10 items-center justify-center rounded-md border bg-transparent whitespace-nowrap transition-colors duration-200 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
         className,
       )}
       {...props}
