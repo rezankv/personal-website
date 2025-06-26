aafdd468493beb13ccd1cbbff5815baa3610e201
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-06-27 00:15:31 +0330

    update notfound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getLocale, getTranslations } from "next-intl/server";
+import { cookies } from "next/headers";
+import { getTranslations, setRequestLocale } from "next-intl/server";
 
 // styles
 import "@app/styles";
@@ -11,14 +11,21 @@ import { routes } from "@app/constants";
 import { Link } from "@app/i18n/navigation";
 import { Locale } from "@app/i18n/routing";
 
+// providers
+import { Providers } from "@app/providers";
+
 const NotfoundPage = async () => {
+  const cookieStore = await cookies();
+  const locale = cookieStore.get("NEXT_LOCALE")?.value as Locale;
+
+  setRequestLocale(locale);
+
   const t = await getTranslations("RootLayout.pages.NotFoundPage");
-  const locale = (await getLocale()) as Locale;
 
   return (
     <html suppressHydrationWarning>
       <body>
-        <NextIntlClientProvider>
+        <Providers>
           <div className="bg-background fixed inset-0 z-10 flex flex-col items-center justify-center gap-2 px-2 py-4 text-center">
             <span className="text-2xl font-bold md:text-4xl">{t("title")}</span>
             <p className="text-muted-foreground">{t("description")}</p>
@@ -30,7 +37,7 @@ const NotfoundPage = async () => {
               {t("back")}
             </Link>
           </div>
-        </NextIntlClientProvider>
+        </Providers>
       </body>
     </html>
   );
