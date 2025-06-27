9cf73101f3a48c6b3b3141208afb9ecf256ad625
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-06-27 17:35:05 +0330

    update manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,13 +2,13 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'سایت شخصی رضا نیک‌روش',
+        name: 'وبسایت شخصی رضا نیک‌روش',
         short_name: 'رضا نیک‌روش',
-        description: 'سایت شخصی رضا نیک‌روش. اینجا پروژه ها و مقالاتم رو با شما به اشتراک میزارم.',
+        description: "وبسایت شخصی رضا نیک‌روش. اینجا پروژه ها و مقالاتم رو با شما به اشتراک میزارم.",
         start_url: '/',
         display: 'standalone',
         "theme_color": "#202830",
-        "background_color": "#312e31",
+        "background_color": "#202830",
         icons: [
             {
                 "src": "/web-app-manifest-192x192.png",
