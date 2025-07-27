1460a68ac23e392dea7d3384a7fe86c5675307fb
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-07-27 18:04:01 +0330

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,32 +1,34 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 // i18n
-import { Locale } from "@app/i18n/routing"
+import { Locale } from "@app/i18n/routing";
 
 // constants
-import { routes } from "@app/constants"
+import { routes } from "@app/constants";
 
 // contents
-import { postService, projectService } from "@app/contents"
-
+import { postService, projectService } from "@app/contents";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const createUrl = (path: string, locale?: Locale,) => {
-        const BASE_URL = `${process.env.WEBSITE_URL}`
-        return `${BASE_URL}${locale ? `/${locale}` : ""}${path === '/' ? '' : path}`
-    }
+    const createUrl = (path: string, locale?: Locale) => {
+        const BASE_URL = `${process.env.WEBSITE_URL}`;
+        return `${BASE_URL}${locale ? `/${locale}` : ""}${path === "/" ? "" : path}`;
+    };
 
-    const allRoutes = [routes.HOME_ROUTE, routes.POSTS_ROUTE, routes.PROJECTS_ROUTE,
-    ...postService.getAll().map(post => routes.SINGLE_POST_ROUTE(post.slug)),
-    ...projectService.getAll().map(project => routes.SINGLE_PROJECT_ROUTE(project.slug))]
+    const allRoutes = [
+        routes.HOME_ROUTE,
+        routes.POSTS_ROUTE,
+        routes.PROJECTS_ROUTE,
+        ...postService.getAll().map((post) => routes.SINGLE_POST_ROUTE(post.slug)),
+        ...projectService
+            .getAll()
+            .map((project) => routes.SINGLE_PROJECT_ROUTE(project.slug)),
+    ];
 
-    return allRoutes.map(route => ({
-        url: createUrl(route), lastModified: new Date(), priority: 1, changeFrequency: "weekly", alternates: {
-            languages: {
-                fa: createUrl(route, 'fa'),
-                en: createUrl(route, 'en')
-            }
-        }
+    return allRoutes.map((route) => ({
+        priority: 1,
+        changeFrequency: "weekly",
+        url: createUrl(route),
+        lastModified: new Date(),
     }))
-
 }
