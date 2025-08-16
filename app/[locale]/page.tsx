5c103664abcff8ce00d143a654141ea3fdd5349b
Reza Nikravesh <reza.nikravesh80@gmail.com>
2025-08-16 16:40:46 +0330

    add latest projects section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -9,7 +9,7 @@ import {
   AboutSection,
   ExperienceSection,
   PostsSection,
-  // ProjectSection,
+  ProjectSection,
 } from "./_components";
 
 export const dynamicParams = false;
@@ -60,7 +60,7 @@ export default async function Home({
   return (
     <div className="animate-fade-in-up mx-4 mt-4 flex flex-col gap-10">
       <AboutSection />
-      {/* <ProjectSection /> */}
+      <ProjectSection />
       <PostsSection />
       <ExperienceSection />
     </div>
